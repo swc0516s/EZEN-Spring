63fadb1677af895c05cd8e9e43c180d342bc7006
swc0516 <swc0516@naver.com>
2022-05-02 16:28:13 +0900

    Spring

---
+++ Spring Workspace Lecture/22_AOPJava/src/main/java/kr/co/ezenac/beans/TestBean1.java
@@ -0,0 +1,14 @@
+package kr.co.ezenac.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBean1 {
+	
+		public void method1() throws Exception{
+			System.out.println("TestBean1의 메서드 호출");
+			
+				
+		}
+
+}
