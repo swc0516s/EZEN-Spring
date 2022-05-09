6189f6d8503ae31b3fa8e678f73583c5767fd421
swc0516 <swc0516@naver.com>
2022-05-09 15:29:00 +0900

    spring

--- Spring Workspace Lecture/24_MyBatis/src/main/java/kr/co/ezenac/mapper/MapperInterface.java
+++ Spring Workspace Lecture/24_MyBatis/src/main/java/kr/co/ezenac/mapper/MapperInterface.java
@@ -2,10 +2,12 @@ package kr.co.ezenac.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import kr.co.ezenac.beans.JdbcBean;
 
@@ -25,4 +27,9 @@ public interface MapperInterface {
 	@Insert("insert into jdbc_table (int_data, str_data)values(#{int_data},#{str_data})")
 	void insert_data(JdbcBean bean);
 	
+	@Update("update jdbc_table set str_data=#{str_data} where int_data=#{int_data}")
+	void update_data(JdbcBean bean);
+	
+	@Delete("delete from jdbc_table where int_data=#{int_data}")
+	void delete_data(int i);
 }
